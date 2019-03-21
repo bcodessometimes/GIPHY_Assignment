2294af0d789c8b0bd2f148b1fa4c81dcf890c659
Rebecca Potter <rebeccapotter@Rebeccas-MacBook-Air.local>
2019-03-21 11:52:13 -0600

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Giphy Assignment
+
+## Instructions
+
+* Click the buttons above to display 10 gifs of the animal. To add a new animal type it into the text box and click submit. This will display 10 gifs and add a button for that animal. 
--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,18 +1,31 @@
 //Global variables
 var itemArray = ["cat", "dog", "mouse", "elephant"];
 var apiKey = "rqcSo1qF6lKTtawP3hwe4gab9J4gh4RG"
-var input = "";
+var input;
+var temp;
+var a;
+var animalImage;
+var animalDiv;
+var i;
 
 //on load clear buttons and populate from itemArray
 function onStart() {
     
     for(var i = 0; i < itemArray.length; i++) {
-        var a = $("<button>"); 
+        a = $("<button>"); 
         a.text(itemArray[i]);
+        a.addClass("static-btn");
         $("#buttonDiv").append(a);
     }
 
 };
+
+
+$("body").on("click", ".static-btn", function (){
+    alert("working");
+    $(".static-btn").val().append(temp);
+    console.log(temp);
+});
 
 $("#button-addon2").on("click", function(){
     $("#buttonDiv").empty();
@@ -24,10 +37,11 @@ $("#button-addon2").on("click", function(){
     onStart();
 });
 
+
 //ajax call
 $("button").on("click", function(){
     $("#gifDiv").empty();
-    var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=rqcSo1qF6lKTtawP3hwe4gab9J4gh4RG&q=` + input + `&limit=10&offset=0&rating=G&lang=en`
+    var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=rqcSo1qF6lKTtawP3hwe4gab9J4gh4RG&q=` + input + `&limit=2&offset=0&rating=G&lang=en`
 
     $.ajax({
         url: queryURL,
@@ -38,24 +52,43 @@ $("button").on("click", function(){
         
        console.log(queryURL);
        console.log(response);
-       var results = response.data;
+       results = response.data;
 
-       for (var i = 0; i < results.length; i++) {
+       for (i = 0; i < results.length; i++) {
 
-        var animalDiv = $("<div>");
+        animalDiv = $("<div>");
 
         var p = $("<p>").text("Rating: " + results[i].rating);
 
-        var animalImage = $("<img>");
+        animalImage = $("<img>");
         animalImage.attr("src", results[i].images.fixed_height.url);
 
         animalDiv.append(p);
         animalDiv.append(animalImage);
 
         $("#gifDiv").prepend(animalDiv);
-      }
-    })
+        
+        
 
+      }
+   
+      
+    })
+    
 });
+
+// $("body").on("click", "img", function(){
+//     alert("works");
+//     animalImage.attr("src", results[i].images.fixed_height_still.url); 
+// })
+// console.log(response);
+
+//response dosent exist outside the function.. I dont know how to build this click event inside my function with my ajax call
+//my thought process is to just change the attribute to the still image on click.
+
+// $("body").on("click", "img", function(){
+//     alert("works");
+//     animalImage.attr("src", results[i].images.fixed_height_still.url); 
+// })
 
 onStart();
